2291c9111304f0aef0f4bcff216eb0b6a11d15e1
dfinazhin <dfinazhin@gmail.com>
2025-08-24 22:29:16 +0500

    Instal MUI and json-server

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css';
 import styled from 'styled-components';
+import LayersTwoToneIcon from '@mui/icons-material/LayersTwoTone';
+import AddTwoToneIcon from '@mui/icons-material/AddTwoTone';
 
 const Div = styled.div`
     text-align: left;
@@ -8,7 +10,12 @@ const Div = styled.div`
 export const App = () => {
     return (
         <div className="card">
-            <i className="fa fa-map"></i>
+            <LayersTwoToneIcon color="primary" sx={{ letterSpacing: '-0.05em' }} />
+            <AddTwoToneIcon
+                color="primary"
+                sx={{ fontSize: 15, letterSpacing: '-0.05em' }}
+            />
+            +<i className="fa fa-map"></i>
             <div>Ghbdtn</div>
             <Div>456</Div>
         </div>
