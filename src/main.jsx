3c90b369168f4cac94fb0ee0329b571491eb2130
dfinazhin <dfinazhin@gmail.com>
2025-08-26 20:10:23 +0500

    Реализовать основной компонент блога с роутингом

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react';
+//import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Blog } from './blog';
 import './index.css';
-import { App } from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
+    //<StrictMode> // https://vladilen.ru/pl/teach/control/lesson/view?id=302435031
+    <BrowserRouter>
+        <Blog />
+    </BrowserRouter>,
+    //</StrictMode>,
 );
